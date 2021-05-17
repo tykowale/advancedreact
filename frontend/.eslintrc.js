33a147a3c562ec9cb49f43db4ac947fade16aa30
tykowale <tykowale@gmail.com>
2021-05-16 19:32:18 -0700

    more basic setup

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules).reduce(
+  (acc, rule) => {
+    acc[`jsx-a11y/${rule}`] = 'off';
+    return acc;
+  },
+  {},
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: ['prettier', 'prettier/react', 'plugin:react/recommended', 'airbnb'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    ...a11yOff,
+    'import/no-named-as-default': 0,
+    'react/prop-types': [1, { skipUndeclared: true }],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 export default function IndexPage() {
   return (
-    <div>
+    <>
       <p>Hello!</p>
-    </div>
+    </>
   );
 }
