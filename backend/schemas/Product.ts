1b74a4f64963999e7d6bfdc2074215fa6d84d6ac
tykowale <tykowale@gmail.com>
2021-05-24 19:39:26 -0700

    two way data binding for product/product image

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,10 +1,19 @@
 import { list } from '@keystone-next/keystone/schema';
-import { integer, select, text } from '@keystone-next/fields';
+import { integer, relationship, select, text } from '@keystone-next/fields';
 
 export const Product = list({
   fields: {
     name: text({ isRequired: true }),
     description: text({ ui: { displayMode: 'textarea' } }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
     status: select({
       options: [
         { label: 'Draft', value: 'DRAFT' },
